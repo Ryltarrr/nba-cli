4ae004ea3b3c50059a0f0f42f19b3d2d2a769364
Justin Guerré <guerrejustin@gmail.com>
2022-05-01 16:44:26 +0200

    add flag parsing

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	dt := time.Now()
 	date := flag.String("date", dt.Format("2006-01-02"), "the date")
-	var fetcher fetcher.Fetcher
+    flag.Parse()
+    var fetcher fetcher.Fetcher
 	fetcher.GetGamesForDate(*date)
 }
