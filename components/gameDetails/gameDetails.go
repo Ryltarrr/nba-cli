645224296442296c1c7f6b89c43abf764d9f7cbd
Justin Guerré <guerrejustin@gmail.com>
2022-06-03 19:18:34 +0200

    better gameDetails

--- components/gameDetails/gameDetails.go
+++ components/gameDetails/gameDetails.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Ryltarrr/nba-cli/parser"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type Model struct {
@@ -26,21 +27,49 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	s := ""
-	s += m.game.GameStatusText + "\n"
-	homePeriods, awayPeriods := "", ""
-	for i := 0; i < m.game.Period; i++ {
-		homePeriods += fmt.Sprintf("%v ", m.game.HomeTeam.Periods[i].Score)
-		awayPeriods += fmt.Sprintf("%v ", m.game.AwayTeam.Periods[i].Score)
-	}
-
-	homePeriods += fmt.Sprintf(" | %v", m.game.HomeTeam.Score)
-	awayPeriods += fmt.Sprintf(" | %v", m.game.AwayTeam.Score)
-	s += homePeriods + "\n" + awayPeriods
-	s += "\n"
-	return s
+	return lipgloss.JoinVertical(lipgloss.Left, m.getPeriods(), m.getLeaders())
 }
 
 func (m *Model) SetGame(g parser.Game) {
 	m.game = g
+}
+
+func (m Model) getPeriods() string {
+	headerStr := lipgloss.NewStyle().Underline(true).Bold(true).Render("Score by quarter:")
+
+	homePeriods, awayPeriods := "", ""
+	for i := 0; i < m.game.Period; i++ {
+		homeStyle := lipgloss.NewStyle().Bold(false)
+		awayStyle := homeStyle.Copy()
+		homeScore := m.game.HomeTeam.Periods[i].Score
+		awayScore := m.game.AwayTeam.Periods[i].Score
+
+		if homeScore > awayScore {
+			homeStyle = homeStyle.Bold(true)
+		} else {
+			awayStyle = awayStyle.Bold(true)
+		}
+
+		homePeriods += homeStyle.Render(fmt.Sprintf("%v ", homeScore))
+		awayPeriods += awayStyle.Render(fmt.Sprintf("%v ", awayScore))
+	}
+
+	return headerStr + "\n" + homePeriods + "\n" + awayPeriods + "\n"
+}
+
+func (m Model) getLeaders() string {
+	headerStr := lipgloss.NewStyle().Underline(true).Bold(true).Render("Best players:")
+	homeLeaderView := leaderView(m.game.GameLeaders.HomeLeaders)
+	awayLeaderView := leaderView(m.game.GameLeaders.AwayLeaders)
+
+	return lipgloss.JoinVertical(lipgloss.Top, headerStr, homeLeaderView, "", awayLeaderView)
+}
+
+func leaderView(player parser.Player) string {
+	bold := lipgloss.NewStyle().Bold(true)
+
+	s := bold.Render("#"+player.JerseyNum) + " " + player.Name + "\n"
+	s += fmt.Sprintf("%v pts, %v rbd, %v ast", player.Points, player.Rebounds, player.Assists)
+
+	return s
 }
